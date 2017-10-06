9e9eb11f6c6602ac44570cfdc2a48630ecebc45d
ecaman <rlferreira94@gmail.com>
2017-10-06 13:31:38 +0200

    Modification pour éviter de bloquer la clef github

--- Lesson3/exo_dom_lesson_03.py
+++ Lesson3/exo_dom_lesson_03.py
@@ -1,7 +1,7 @@
 
 # coding: utf-8
 
-# In[69]:
+# In[1]:
 
 
 from bs4 import *
@@ -20,7 +20,7 @@ import time
 import matplotlib.pyplot as plt
 
 
-# In[70]:
+# In[2]:
 
 
 path = "https://gist.github.com/paulmillr/2657075"
@@ -36,7 +36,7 @@ soup_td = BeautifulSoup(text, 'html.parser')
 extracted_contributors = soup_td.findAll('a', {"href": re.compile("https://github.com/*")})
 
 
-# In[71]:
+# In[3]:
 
 
 contributors = []
@@ -45,25 +45,34 @@ for contributor in extracted_contributors:
 contributors = contributors[:256]
 
 
-# In[72]:
+# In[24]:
+
+
+global token 
+token = input('Github Access token: ')
+
+
+# In[25]:
 
 
 # Fonction to get mean of stars for all the repositories owned of a github user:
 def get_mean_stars(github_user):
+    
     print('Counting stars for a new contributor')
-    url_repositories = "https://api.github.com/users/" + github_user
+    url_repositories = "https://api.github.com/users/" + github_user + "?access_token=" + token
     # Ajout d'un sleep pour éviter d'être bloqué par l'api du type:
     # You have triggered an abuse detection mechanism
     time.sleep(1)
-    response_user_repositories = requests.get(url=url_repositories, auth = (username, password)).json()
+    response_user_repositories = requests.get(url=url_repositories).json()#, auth = (username, password)).json()
     repositories_owned = response_user_repositories["public_repos"]
     
     number_of_pages = math.ceil(repositories_owned/100)
     response_user_stars = []
     stars = 0
     for i in range(1, number_of_pages + 1):
-        url_stars = "https://api.github.com/users/" + contributors[2] + "/repos?page=" + str(i) + "&per_page=100"
-        response_user_stars += requests.get(url=url_stars, auth = (username, password)).json()
+        url_stars = "https://api.github.com/users/" + github_user + "/repos?access_token=" + token +"&page=" +str(i)+"&per_page=100"
+        #url_stars = "https://api.github.com/users/" + contributors[2] + "/repos?page=" + str(i) + "&per_page=100"
+        response_user_stars += requests.get(url=url_stars).json()#, auth = (username, password)).json()
     for i in range(0, len(response_user_stars)):
         stars = stars + response_user_stars[i]["stargazers_count"]
     if(repositories_owned != 0):       
@@ -74,30 +83,25 @@ def get_mean_stars(github_user):
     return mean_of_stars
 
 
-# In[73]:
+# In[26]:
 
 
-username = input('Github username: ')
-password = getpass.getpass('Github password: ')
-
-
-# In[74]:
-
-
+#from functools import partial
+#from itertools import product
 # Pour augmenter la vitesse d'execution on peut augmenter le nombre de process.
 # Cependant si on va trop vite l'api va nous bloquer.
-with Pool(processes=multiprocessing.cpu_count()) as pool:
+with Pool(processes=4) as pool: #multiprocessing.cpu_count()
     list_of_means = pool.map(get_mean_stars, contributors)
 
 
-# In[75]:
+# In[61]:
 
 
 df_contributors_stars = pd.DataFrame({"Contributors": contributors, "Mean_of_stars":list_of_means})
 df_contributors_stars.head(20)
 
 
-# In[76]:
+# In[62]:
 
 
 # Vérification que les threads n'ont pas tout mélangé, on prend un user au hasard. Et on vérifie qu'on trouve les mêmes valeurs
@@ -106,7 +110,7 @@ print(df_contributors_stars.loc[[r]])
 print(contributors[r], get_mean_stars(contributors[r]))
 
 
-# In[79]:
+# In[63]:
 
 
 # Classement des users de la plus grande moyenne à la plus petite:
@@ -124,4 +128,11 @@ plt.show()
 #for github_user in contributors:
 #    mean_of_stars.append(get_mean_stars(github_user))
 #mean_of_stars
+
+
+# In[53]:
+
+
+#username = input('Github username: ')
+#password = getpass.getpass('Github password: ')
 
